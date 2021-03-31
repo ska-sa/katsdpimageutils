559428f672b5b345f33300b7cfeb500cc358ffd0
SihlanguI <isihlangu@ska.ac.za>
2021-03-31 00:25:29 +0000

    made changes to use katbeam models

--- scripts/fits-primarybeamimage.py
+++ scripts/fits-primarybeamimage.py
@@ -3,7 +3,7 @@ import argparse
 import logging
 import os
 
-import katsdpimageutils.primary_beam_correction as pbc
+from primary_beam_correction import Circular_Beam
 
 
 def initialize_logs():
@@ -35,12 +35,19 @@ def main():
     outpath = args.output
     logging.info('----------------------------------------')
     logging.info('Getting the beam pattern for each frequency plane based on the '
-                 'Cosine-squared power approximation from Mauch et al. (2020).')
-    bp, raw_image = pbc.beam_pattern(path)
+                 'katbeam module (https://github.com/ska-sa/katbeam.git).')
+    circbeam = Circular_Beam()
+    # read in fits image and check the band type
+    raw_image = circbeam.read_fits(path)
+    if round(raw_image.header['CRVAL3']/1e6) == 1284:  # L-band
+        circbeam = Circular_Beam('MKAT-AA-L-JIM-2020')
+    if round(raw_image.header['CRVAL3']/1e6) == 816:  # UHF-band
+        circbeam = Circular_Beam('MKAT-AA-UHF-JIM-2020')
+    bp = circbeam.beam_pattern(path)
     # pbc - primary beam corrected
     logging.info('----------------------------------------')
     logging.info('Doing the primary beam correction in each frequency plane and averaging')
-    pbc_image = pbc.primary_beam_correction(bp, raw_image, px_cut=0.1)
+    pbc_image = circbeam.primary_beam_correction(bp, raw_image, px_cut=0.1)
     logging.info('----------------------------------------')
     logging.info('Saving the primary beam corrected image')
     fpath, fext = os.path.splitext(path)
@@ -48,7 +55,7 @@ def main():
         output_path = fpath + '_PB' + fext
     else:
         output_path = outpath
-    pbc.write_new_fits(pbc_image, path, outputFilename=output_path)
+    circbeam.write_new_fits(pbc_image, path, outputFilename=output_path)
     logging.info('------------------DONE-------------------')
 
 
