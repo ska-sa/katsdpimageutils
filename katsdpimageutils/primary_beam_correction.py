22da7cfa657390cda94ad9faa12d4012d5270980
SihlanguI <isihlangu@ska.ac.za>
2021-04-08 12:40:03 +0000

    fixed the mistake of making all the module functions into class methods

--- katsdpimageutils/primary_beam_correction.py
+++ katsdpimageutils/primary_beam_correction.py
@@ -6,8 +6,7 @@ from astropy.io import fits
 from astropy import wcs
 from astropy import coordinates
 
-import katbeam as katbeam
-from katbeam import JimBeam
+import katbeam
 
 
 _cosine_taper = katbeam.jimbeam._cosine_taper
@@ -27,94 +26,263 @@ def _circular_pattern(x, y, fwhm_x, fwhm_y):
     return _cosine_taper(rr)
 
 
-class Circular_Beam(JimBeam):
-    """MeerKAT simplified circular primary beam models for L and UHF bands.
+def read_fits(path):
+    """Read in the FITS file.
 
-    This is implementation is based on the katbeam module.
-    https://github.com/ska-sa/katbeam.git
+    Parameters
+    ----------
+    path : str
+        FITS file
+
+    Returns
+    -------
+    output_file : astropy.io.fits.hdu.image.PrimaryHDU
+        First element of the HDU list
     """
-    def read_fits(self, path):
-        """Read in the FITS file.
+    fl = fits.open(path)
+    images = fl[0]
+    return images
 
-        Parameters
-        ----------
-        path : str
-            FITS file
 
-        Returns
-        -------
-        output_file : astropy.io.fits.hdu.image.PrimaryHDU
-            First element of the HDU list
-        """
-        fl = fits.open(path)
-        images = fl[0]
-        return images
+def get_position(path):
+    """Determine the sky coordinate of the pointing centre.
 
-    def get_position(self, path):
-        """Determine the sky coordinate of the pointing centre.
+    This implementation assumes that the pointing centre is the
+    same as the crval.
 
-        This implementation assumes that the pointing centre is the
-        same as the crval.
+    Parameters
+    ----------
+    path : str
+        FITS file
 
-        Parameters
-        ----------
-        path : str
-            FITS file
+    Returns
+    -------
+    position : astropy.coordinations.SkyCoord
+        Sky coordinate of the phase centre
+    image_wcs : astropy.wcs.wcs.WCS
+        WCS keywords in the primary HDU
+    """
+    # Parse the WCS keywords in the primary HDU
+    image_wcs = wcs.WCS(path)
+    # Get pointing centre of the observation
+    phase_centre_ra = image_wcs.celestial.wcs.crval[0]
+    phase_centre_dec = image_wcs.celestial.wcs.crval[1]
+    # Convert to astropy.coordinates.SkyCoord object
+    phase_center = coordinates.SkyCoord(phase_centre_ra, phase_centre_dec, unit=(u.deg, u.deg))
+    return phase_center, image_wcs
 
-        Returns
-        -------
-        position : astropy.coordinations.SkyCoord
-            Sky coordinate of the phase centre
-        image_wcs : astropy.wcs.wcs.WCS
-            WCS keywords in the primary HDU
-        """
-        # Parse the WCS keywords in the primary HDU
-        image_wcs = wcs.WCS(path)
-        # Get pointing centre of the observation
-        phase_centre_ra = image_wcs.celestial.wcs.crval[0]
-        phase_centre_dec = image_wcs.celestial.wcs.crval[1]
-        # Convert to astropy.coordinates.SkyCoord object
-        phase_center = coordinates.SkyCoord(phase_centre_ra, phase_centre_dec, unit=(u.deg, u.deg))
-        return phase_center, image_wcs
 
-    def get_coordinates(self, phase_center, image_wcs):
-        """Compute the coordinates where beam is sampled, in degrees.
+def get_coordinates(image_wcs):
+    """Compute the coordinates where beam is sampled, in degrees.
 
-        Parameters
-        ----------
-        phase_center : astropy.coordinations.SkyCoord
-            phase center position
-        image_wcs : astropy.wcs.wcs.WCS
-            WCS keywords in the primary HDU
-        """
-        # Get pixel coordinates
-        row, col = np.indices((image_wcs.array_shape[2], image_wcs.array_shape[3]))
-        p2w = image_wcs.pixel_to_world(col, row, 0, 0)[0]
-        separation_deg = p2w.separation(phase_center).deg
-        x, y = np.meshgrid(separation_deg[0], separation_deg[1])
-        return x, y
+    Parameters
+    ----------
+    image_wcs : astropy.wcs.wcs.WCS
+        WCS keywords in the primary HDU
+    """
+    beamextent = 2
+    margin = np.linspace(-beamextent/2, beamextent/2, image_wcs.pixel_shape[0])
+    x, y = np.meshgrid(margin, margin)
+    return x, y
 
-    def central_freq(self, path):
-        """Determine central frequency of each frequency plane.
 
-        Parameters
-        ----------
-        path : str
-            FITS file
+def central_freq(path):
+    """Determine central frequency of each frequency plane.
 
-        Returns
-        -------
-        output : numpy array
-            An array of central frequencie in MHz of each frequency plane.
-        """
-        images = self.read_fits(path)
-        c_freq_plane = []
-        # NSPEC -  number of frequency planes.
-        for i in range(1, images.header['NSPEC']+1):
-            # FREQ00X is the central frequency for each plane X.
-            c_freq_plane.append(images.header['FREQ{0:04}'.format(i)])
-        return np.array(c_freq_plane)
+    Parameters
+    ----------
+    path : str
+        FITS file
 
+    Returns
+    -------
+    output : numpy array
+        An array of central frequencie in MHz of each frequency plane.
+    """
+    images = read_fits(path)
+    c_freq_plane = []
+    # NSPEC -  number of frequency planes.
+    for i in range(1, images.header['NSPEC']+1):
+        # FREQ00X is the central frequency for each plane X.
+        c_freq_plane.append(images.header['FREQ{0:04}'.format(i)])
+    return np.array(c_freq_plane)
+
+
+def cosine_power_pattern(x, y, path, c_freq):
+    """Compute Power patterns for a given frequency.
+
+    This uses the katbeam module.
+    https://github.com/ska-sa/katbeam.git
+
+    Parameters
+    ----------
+    x, y : arrays of float of the same shape
+        Coordinates where beam is sampled, in degrees
+    path : str
+        FITS file
+    c_freq : numpy array
+        An array of central frequencies for each frequency plane
+    """
+    # read in fits image and check the band type
+    raw_image = read_fits(path)
+    if round(raw_image.header['CRVAL3']/1e6) == 1284:  # L-band
+        circbeam = CircularBeam('MKAT-AA-L-JIM-2020')
+    if round(raw_image.header['CRVAL3']/1e6) == 816:  # UHF-band
+        circbeam = CircularBeam('MKAT-AA-UHF-JIM-2020')
+    flux_density = []
+    for nu in c_freq:
+        nu = nu/1.e6  # GHz to MHz
+        a_b = circbeam.I(x, y, nu)
+        flux_density.append(a_b.ravel())
+    return flux_density
+
+
+def beam_pattern(path):
+    """Get beam pattern from katbeam module.
+
+    Parameters
+    ----------
+    path : str
+        FITS file
+    """
+    # Get the central frequency of the image header given.
+    c_freq = central_freq(path)
+    # Get radial separation between sources and the phase centre
+    phase_center, image_wcs = get_position(path)
+    x, y = get_coordinates(image_wcs)
+    beam_list = cosine_power_pattern(x, y, path, c_freq)
+    return beam_list
+
+
+def standard_deviation(data):
+    """Compute the median and the estimate of the standard deviation.
+
+    This is based on the median absolute deviation (MAD).
+    """
+    MAD_TO_SD = 1.4826
+    med = np.nanmedian(data)
+    dev = np.abs(data - med)
+    return med, MAD_TO_SD * np.nanmedian(dev)
+
+
+def inverse_variance(data):
+    """Calculate the inverse variance.
+
+    Reject pixels more than 5 sigma from the median and reject all zeros
+    until either no more pixels are rejected or a maximum of 50 iterations
+    is reached.
+    """
+    data = data[(data != 0.0) & (np.isfinite(data))]
+    if len(data) == 0:
+        return 0.0
+    med, sd = standard_deviation(data)
+    for i in range(50):
+        old_sd = sd
+        cut = np.abs(data - med) < 5.0 * sd
+        if np.all(~cut):
+            return 1/(sd)**2
+        data = data[cut]
+        med, sd = standard_deviation(data)
+        if sd == 0.0:
+            return 1/(old_sd)**2
+    return 1/(sd)**2
+
+
+def weighted_average(arr, weights):
+    """Compute weighted average of all the frequency planes."""
+    wt_average = np.average(arr, weights=weights, axis=0)
+    return wt_average
+
+
+def primary_beam_correction(beam_pattern, raw_image, px_cut=0.1):
+    """Correct the effects of primary beam.
+
+    Parameters
+    ----------
+    beam_pattern : numpy array
+        Array of beam pattern
+    raw_image : astropy.io.fits.hdu.image.PrimaryHDU
+        First element of the HDU list
+    px_cut : float
+       Threshold to cut off all the pixels with attenuated flux less than
+       the value.
+    """
+    nterm = raw_image.header['NTERM']
+    weight = []
+    pbc_image = []
+    # Get all the pixels with attenuated flux of less than 10% of the peak
+    beam_mask = beam_pattern[-1] <= px_cut
+    for i in range(len(beam_pattern)):
+        # Blank all the pixels with attenuated flux of less than 10% of the peak
+        beam_pattern[i][beam_mask] = np.nan
+        # Get the inverse variance (weight) in each frequency plane
+        # (before primary beam correction)
+        weight.append(inverse_variance(np.ravel(raw_image.data[0, i+nterm, :, :])))
+        # correct the effect of the beam by dividing with the beam pattern.
+        ratio = np.ravel(raw_image.data[0, i + nterm, :, :]) / beam_pattern[i]
+        pbc_image.append(ratio)
+    # Convert primary beam corrected (pbc) and weight list into numpy array
+    pbc_image = np.array(pbc_image)
+    weight = np.array(weight)
+    # Calculate a weighted average from the frequency plane images
+    corr_image = weighted_average(pbc_image, weight)
+    # Add new axis
+    corr_image = corr_image.reshape(1, 1, raw_image.data.shape[2], raw_image.data.shape[3])
+    return corr_image
+
+
+def _get_value_from_history(keyword, header):
+    """
+    Return the value of a keyword from the FITS HISTORY in header.
+
+    Assumes keyword is found in a line of the HISTORY with format: 'keyword = value'.
+
+    Parameters
+    ----------
+    keyword : str
+          keyword to search for such as BMAJ, CLEANBMJ and BMIN
+    header : astropy header
+          Image header
+    """
+    for history in header['HISTORY']:
+        line = history.replace('=', ' ').split()
+        try:
+            ind = line.index(keyword)
+        except ValueError:
+            continue
+        return line[ind + 1]
+    raise KeyError(f'{keyword} not found in HISTORY')
+
+
+def write_new_fits(pbc_image, path, outputFilename):
+    """
+    Write out a new FITS image with primary beam corrected continuum in its first plane.
+    """
+    images = read_fits(path)
+    hdr = images.header
+    newhdr = hdr.copy()
+    # change the frequency plane keywords, we don't want multiple frequency axes
+    newhdr['CTYPE3'] = 'FREQ'
+    newhdr['NAXIS3'] = 1
+    newhdr['CDELT3'] = 1.0
+    try:
+        if 'CLEANBMJ' in newhdr and newhdr['CLEANBMJ'] > 0:
+            # add in required beam keywords
+            newhdr['BMAJ'] = newhdr['CLEANBMJ']
+            newhdr['BMIN'] = newhdr['CLEANBMN']
+            newhdr['BPA'] = newhdr['CLEANBPA']
+        else:
+            # Check CLEANBMAJ in the history
+            newhdr['BMAJ'] = float(_get_value_from_history('BMAJ', newhdr))
+            newhdr['BMIN'] = float(_get_value_from_history('BMIN', newhdr))
+            newhdr['BPA'] = float(_get_value_from_history('BPA', newhdr))
+    except KeyError:
+        logging.error('Exception occurred, keywords not found', exc_info=True)
+    new_hdu = fits.PrimaryHDU(header=newhdr, data=pbc_image)
+    return new_hdu.writeto(outputFilename, overwrite=True)
+
+
+class CircularBeam(katbeam.JimBeam):
     def HH(self, x, y, freqMHz):
         """Calculate the H co-polarised beam at the provided coordinates.
 
@@ -150,161 +318,3 @@ class Circular_Beam(JimBeam):
         """
         squint, fwhm = self._interp_squint_fwhm(freqMHz)
         return _circular_pattern(x, y, fwhm[2], fwhm[3])
-
-    def cosine_power_pattern(self, x, y, c_freq):
-        """Compute Power patterns for a given frequency.
-
-        This uses the katbeam module.
-        https://github.com/ska-sa/katbeam.git
-
-        Parameters
-        ----------
-        x, y : arrays of float of the same shape
-            Coordinates where beam is sampled, in degrees
-        c_freq : numpy array
-            An array of central frequencies for each frequency plane
-        """
-        flux_density = []
-        for nu in c_freq:
-            nu = nu/1.e6  # GHz to MHz
-            a_b = self.I(x, y, nu)
-            flux_density.append(a_b.ravel())
-        return flux_density
-
-    def beam_pattern(self, path):
-        """Get beam pattern from katbeam module.
-
-        Parameters
-        ----------
-        path : str
-            FITS file
-        """
-        # Get the central frequency of the image header given.
-        c_freq = self.central_freq(path)
-        # Get radial separation between sources and the phase centre
-        phase_center, image_wcs = self.get_position(path)
-        x, y = self.get_coordinates(phase_center, image_wcs)
-        beam_list = self.cosine_power_pattern(x, y, c_freq)
-        return beam_list
-
-    def standard_deviation(self, data):
-        """Compute the median and the estimate of the standard deviation.
-
-        This is based on the median absolute deviation (MAD).
-        """
-        MAD_TO_SD = 1.4826
-        med = np.nanmedian(data)
-        dev = np.abs(data - med)
-        return med, MAD_TO_SD * np.nanmedian(dev)
-
-    def inverse_variance(self, data):
-        """Calculate the inverse variance.
-
-        Reject pixels more than 5 sigma from the median and reject all zeros
-        until either no more pixels are rejected or a maximum of 50 iterations
-        is reached.
-        """
-        data = data[(data != 0.0) & (np.isfinite(data))]
-        if len(data) == 0:
-            return 0.0
-        med, sd = self.standard_deviation(data)
-        for i in range(50):
-            old_sd = sd
-            cut = np.abs(data - med) < 5.0 * sd
-            if np.all(~cut):
-                return 1/(sd)**2
-            data = data[cut]
-            med, sd = self.standard_deviation(data)
-            if sd == 0.0:
-                return 1/(old_sd)**2
-        return 1/(sd)**2
-
-    def weighted_average(self, arr, weights):
-        """Compute weighted average of all the frequency planes."""
-        wt_average = np.average(arr, weights=weights, axis=0)
-        return wt_average
-
-    def primary_beam_correction(self, beam_pattern, raw_image, px_cut=0.1):
-        """Correct the effects of primary beam.
-
-        Parameters
-        ----------
-        beam_pattern : numpy array
-            Array of beam pattern
-        raw_image : astropy.io.fits.hdu.image.PrimaryHDU
-            First element of the HDU list
-        px_cut : float
-           Threshold to cut off all the pixels with attenuated flux less than
-           the value.
-        """
-        nterm = raw_image.header['NTERM']
-        weight = []
-        pbc_image = []
-        # Get all the pixels with attenuated flux of less than 10% of the peak
-        beam_mask = beam_pattern[-1] <= px_cut
-        for i in range(len(beam_pattern)):
-            # Blank all the pixels with attenuated flux of less than 10% of the peak
-            beam_pattern[i][beam_mask] = np.nan
-            # Get the inverse variance (weight) in each frequency plane
-            # (before primary beam correction)
-            weight.append(self.inverse_variance(np.ravel(raw_image.data[0, i+nterm, :, :])))
-            # correct the effect of the beam by dividing with the beam pattern.
-            ratio = np.ravel(raw_image.data[0, i + nterm, :, :]) / beam_pattern[i]
-            pbc_image.append(ratio)
-        # Convert primary beam corrected (pbc) and weight list into numpy array
-        pbc_image = np.array(pbc_image)
-        weight = np.array(weight)
-        # Calculate a weighted average from the frequency plane images
-        corr_image = self.weighted_average(pbc_image, weight)
-        # Add new axis
-        corr_image = corr_image.reshape(1, 1, raw_image.data.shape[2], raw_image.data.shape[3])
-        return corr_image
-
-    def _get_value_from_history(self, keyword, header):
-        """
-        Return the value of a keyword from the FITS HISTORY in header.
-
-        Assumes keyword is found in a line of the HISTORY with format: 'keyword = value'.
-
-        Parameters
-        ----------
-        keyword : str
-              keyword to search for such as BMAJ, CLEANBMJ and BMIN
-        header : astropy header
-              Image header
-        """
-        for history in header['HISTORY']:
-            line = history.replace('=', ' ').split()
-            try:
-                ind = line.index(keyword)
-            except ValueError:
-                continue
-            return line[ind + 1]
-        raise KeyError(f'{keyword} not found in HISTORY')
-
-    def write_new_fits(self, pbc_image, path, outputFilename):
-        """
-        Write out a new FITS image with primary beam corrected continuum in its first plane.
-        """
-        images = self.read_fits(path)
-        hdr = images.header
-        newhdr = hdr.copy()
-        # change the frequency plane keywords, we don't want multiple frequency axes
-        newhdr['CTYPE3'] = 'FREQ'
-        newhdr['NAXIS3'] = 1
-        newhdr['CDELT3'] = 1.0
-        try:
-            if 'CLEANBMJ' in newhdr and newhdr['CLEANBMJ'] > 0:
-                # add in required beam keywords
-                newhdr['BMAJ'] = newhdr['CLEANBMJ']
-                newhdr['BMIN'] = newhdr['CLEANBMN']
-                newhdr['BPA'] = newhdr['CLEANBPA']
-            else:
-                # Check CLEANBMAJ in the history
-                newhdr['BMAJ'] = float(self._get_value_from_history('BMAJ', newhdr))
-                newhdr['BMIN'] = float(self._get_value_from_history('BMIN', newhdr))
-                newhdr['BPA'] = float(self._get_value_from_history('BPA', newhdr))
-        except KeyError:
-            logging.error('Exception occurred, keywords not found', exc_info=True)
-        new_hdu = fits.PrimaryHDU(header=newhdr, data=pbc_image)
-        return new_hdu.writeto(outputFilename, overwrite=True)
--- scripts/fits-primarybeamimage.py
+++ scripts/fits-primarybeamimage.py
@@ -3,7 +3,7 @@ import argparse
 import logging
 import os
 
-from katsdpimageutils.primary_beam_correction import Circular_Beam
+import katsdpimageutils.primary_beam_correction as pbc
 
 
 def initialize_logs():
@@ -36,18 +36,12 @@ def main():
     logging.info('----------------------------------------')
     logging.info('Getting the beam pattern for each frequency plane based on the '
                  'katbeam module (https://github.com/ska-sa/katbeam.git).')
-    circbeam = Circular_Beam()
-    # read in fits image and check the band type
-    raw_image = circbeam.read_fits(path)
-    if round(raw_image.header['CRVAL3']/1e6) == 1284:  # L-band
-        circbeam = Circular_Beam('MKAT-AA-L-JIM-2020')
-    if round(raw_image.header['CRVAL3']/1e6) == 816:  # UHF-band
-        circbeam = Circular_Beam('MKAT-AA-UHF-JIM-2020')
-    bp = circbeam.beam_pattern(path)
+    bp = pbc.beam_pattern(path)
     # pbc - primary beam corrected
     logging.info('----------------------------------------')
     logging.info('Doing the primary beam correction in each frequency plane and averaging')
-    pbc_image = circbeam.primary_beam_correction(bp, raw_image, px_cut=0.1)
+    raw_image = pbc.read_fits(path)
+    pbc_image = pbc.primary_beam_correction(bp, raw_image, px_cut=0.1)
     logging.info('----------------------------------------')
     logging.info('Saving the primary beam corrected image')
     fpath, fext = os.path.splitext(path)
@@ -55,7 +49,7 @@ def main():
         output_path = fpath + '_PB' + fext
     else:
         output_path = outpath
-    circbeam.write_new_fits(pbc_image, path, outputFilename=output_path)
+    pbc.write_new_fits(pbc_image, path, outputFilename=output_path)
     logging.info('------------------DONE-------------------')
 
 
